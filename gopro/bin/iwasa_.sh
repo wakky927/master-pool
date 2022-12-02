6bc5c0457cfe1fd93fc1e0cbd8cac42936eca7a8
takuya <wakky.927.5831@gmail.com>
2022-12-02 17:57:51 +0900

    a

--- gopro/bin/iwasa_.sh
+++ gopro/bin/iwasa_.sh
@@ -2,25 +2,33 @@
 
 cd ../
 
-python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010042/" "/media/takuya/HDD_M2/M2/result/pool/gopro/2/" 250 500 &
-python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010042/" "/media/takuya/HDD_M2/M2/result/pool/gopro/2/" 750 1000 &
-python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010042/" "/media/takuya/HDD_M2/M2/result/pool/gopro/2/" 1250 1500 &
-python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010042/" "/media/takuya/HDD_M2/M2/result/pool/gopro/2/" 1750 2000 &
-python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010042/" "/media/takuya/HDD_M2/M2/result/pool/gopro/2/" 2250 2500 &
-python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010042/" "/media/takuya/HDD_M2/M2/result/pool/gopro/2/" 2750 3000 &
-python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010042/" "/media/takuya/HDD_M2/M2/result/pool/gopro/2/" 3250 3500 &
-python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010042/" "/media/takuya/HDD_M2/M2/result/pool/gopro/2/" 3750 3900 &
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010042/" "/media/takuya/HDD_M2/M2/result/pool/gopro/2/" 0 325 &
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010042/" "/media/takuya/HDD_M2/M2/result/pool/gopro/2/" 325 650 &
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010042/" "/media/takuya/HDD_M2/M2/result/pool/gopro/2/" 650 975 &
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010042/" "/media/takuya/HDD_M2/M2/result/pool/gopro/2/" 975 1300 &
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010042/" "/media/takuya/HDD_M2/M2/result/pool/gopro/2/" 1300 1625 &
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010042/" "/media/takuya/HDD_M2/M2/result/pool/gopro/2/" 1625 1950 &
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010042/" "/media/takuya/HDD_M2/M2/result/pool/gopro/2/" 1950 2275 &
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010042/" "/media/takuya/HDD_M2/M2/result/pool/gopro/2/" 2275 2600 &
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010042/" "/media/takuya/HDD_M2/M2/result/pool/gopro/2/" 2600 2925 &
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010042/" "/media/takuya/HDD_M2/M2/result/pool/gopro/2/" 2925 3250 &
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010042/" "/media/takuya/HDD_M2/M2/result/pool/gopro/2/" 3250 3575 &
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010042/" "/media/takuya/HDD_M2/M2/result/pool/gopro/2/" 3575 3900 &
 
 wait
 
-python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010044/" "/media/takuya/HDD_M2/M2/result/pool/gopro/4/" 250 500 &
-python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010044/" "/media/takuya/HDD_M2/M2/result/pool/gopro/4/" 750 1000 &
-python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010044/" "/media/takuya/HDD_M2/M2/result/pool/gopro/4/" 1250 1500 &
-python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010044/" "/media/takuya/HDD_M2/M2/result/pool/gopro/4/" 1750 2000 &
-python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010044/" "/media/takuya/HDD_M2/M2/result/pool/gopro/4/" 2250 2500 &
-python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010044/" "/media/takuya/HDD_M2/M2/result/pool/gopro/4/" 2750 3000 &
-python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010044/" "/media/takuya/HDD_M2/M2/result/pool/gopro/4/" 3250 3500 &
-python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010044/" "/media/takuya/HDD_M2/M2/result/pool/gopro/4/" 3750 3900 &
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010045/" "/media/takuya/HDD_M2/M2/result/pool/gopro/5/" 2100 2250 &
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010045/" "/media/takuya/HDD_M2/M2/result/pool/gopro/5/" 2250 2400 &
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010045/" "/media/takuya/HDD_M2/M2/result/pool/gopro/5/" 2400 2550 &
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010045/" "/media/takuya/HDD_M2/M2/result/pool/gopro/5/" 2550 2700 &
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010045/" "/media/takuya/HDD_M2/M2/result/pool/gopro/5/" 2700 2850 &
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010045/" "/media/takuya/HDD_M2/M2/result/pool/gopro/5/" 2850 3000 &
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010045/" "/media/takuya/HDD_M2/M2/result/pool/gopro/5/" 3000 3150 &
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010045/" "/media/takuya/HDD_M2/M2/result/pool/gopro/5/" 3150 3300 &
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010045/" "/media/takuya/HDD_M2/M2/result/pool/gopro/5/" 3300 3450 &
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010045/" "/media/takuya/HDD_M2/M2/result/pool/gopro/5/" 3450 3600 &
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010045/" "/media/takuya/HDD_M2/M2/result/pool/gopro/5/" 3600 3750 &
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010045/" "/media/takuya/HDD_M2/M2/result/pool/gopro/5/" 3750 3900 &
 
 wait
 
