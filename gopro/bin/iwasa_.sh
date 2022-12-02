e73b8c57e873002a34116b37c8e82c77c965d118
takuya <wakky.927.5831@gmail.com>
2022-12-02 17:50:03 +0900

    a

---
+++ gopro/bin/iwasa_.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cd ../
+
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010042/" "/media/takuya/HDD_M2/M2/result/pool/gopro/2/" 250 500 &
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010042/" "/media/takuya/HDD_M2/M2/result/pool/gopro/2/" 750 1000 &
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010042/" "/media/takuya/HDD_M2/M2/result/pool/gopro/2/" 1250 1500 &
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010042/" "/media/takuya/HDD_M2/M2/result/pool/gopro/2/" 1750 2000 &
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010042/" "/media/takuya/HDD_M2/M2/result/pool/gopro/2/" 2250 2500 &
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010042/" "/media/takuya/HDD_M2/M2/result/pool/gopro/2/" 2750 3000 &
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010042/" "/media/takuya/HDD_M2/M2/result/pool/gopro/2/" 3250 3500 &
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010042/" "/media/takuya/HDD_M2/M2/result/pool/gopro/2/" 3750 3900 &
+
+wait
+
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010044/" "/media/takuya/HDD_M2/M2/result/pool/gopro/4/" 250 500 &
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010044/" "/media/takuya/HDD_M2/M2/result/pool/gopro/4/" 750 1000 &
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010044/" "/media/takuya/HDD_M2/M2/result/pool/gopro/4/" 1250 1500 &
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010044/" "/media/takuya/HDD_M2/M2/result/pool/gopro/4/" 1750 2000 &
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010044/" "/media/takuya/HDD_M2/M2/result/pool/gopro/4/" 2250 2500 &
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010044/" "/media/takuya/HDD_M2/M2/result/pool/gopro/4/" 2750 3000 &
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010044/" "/media/takuya/HDD_M2/M2/result/pool/gopro/4/" 3250 3500 &
+python3 fugafuga.py "/media/takuya/HDD_M2/M2/result/pool/gopro/tracer_iwasa/GX010044/" "/media/takuya/HDD_M2/M2/result/pool/gopro/4/" 3750 3900 &
+
+wait
+
+echo "all program fin."
